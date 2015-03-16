b536562f89dd865d26ec328292f27ce7805b9413
AlejandroAgroba <alejandro.agroba@gmail.com>
2015-03-16 14:23:39 +0100

    Comprobando si existen las carpetas del usuario y la creamos

--- alta.py
+++ alta.py
@@ -12,3 +12,9 @@ from random import choice
 usuario=sys.argv[1]
 dominio=sys.argv[1]
 
+#Primero comprobamos si el usuario y el dominio existen.
+#En caso de que no creamos la carpeta
+#Si existen lanzamos el error
+
+if os.path.isdir("/var/www/%s" % (usuario)) == False and os.path.isfile("/etc/apache2/sites-available/%s" % (dominio)) == False:
+os.system("mkdir /var/www/%s" % (usuario))
