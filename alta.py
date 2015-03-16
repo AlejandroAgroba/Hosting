1f4ddf4407516d173e77e6b65fe289a34e1a0ea0
AlejandroAgroba <alejandro.agroba@gmail.com>
2015-03-16 14:01:19 +0100

    Preguntamos por el usuario y el dominio

---
+++ alta.py
@@ -0,0 +1,14 @@
+# * coding: utf8 *
+
+import sys
+import os
+import MySQLdb
+import string
+from random import choice
+
+
+#Preguntamos por el nombre de ususario y dominio
+
+usuario=sys.argv[1]
+dominio=sys.argv[1]
+
